1351ad384fa7f62989e9256f61e683355482a225
Javier Manobanda <51manobandajavier@gmail.com>
2022-03-02 17:38:20 -0500

    fix bug vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://javimiot.github.io/',
+  base: 'https://javimiot.github.io/gitactions-react/',
   plugins: [react()]
 })
